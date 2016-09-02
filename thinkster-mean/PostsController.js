60be4727e27e1cd330f6f44ec21ec0e53e909c94
Hanif Fakira <fakira.work@gmail.com>
2016-09-02 16:19:30 +0100

    thinkster-mean: add comment in post page
    add bootstrap
    autofocus on input field

--- thinkster-mean/PostsController.js
+++ thinkster-mean/PostsController.js
@@ -3,9 +3,20 @@ hadditApp.controller('PostsCtrl', [
 '$stateParams',
 'postFactory',
 function($scope, $stateParams, postFactory){
-  $scope.test ='werwqrqr';
   $scope.post = postFactory.posts[$stateParams.id];
+
   $scope.incrementUpvotes = function(comment) {
     comment.upvotes++;
   };
+
+  $scope.addComment = function() {
+    if($scope.body === '') { return; }
+
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'Annoymous',
+      upvotes: 0
+    });
+    $scope.body = '';
+    };
 }]);
