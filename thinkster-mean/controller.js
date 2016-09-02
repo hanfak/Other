9f27c7893c23b9add0e20dbd1d6d7c041581a93a
Hanif Fakira <fakira.work@gmail.com>
2016-09-02 07:15:37 +0100

    thinkster-mean: created postFactory
    extracted data from posts to put in to factory, to be able share
    amongst other controllers

--- thinkster-mean/controller.js
+++ thinkster-mean/controller.js
@@ -1,21 +1,17 @@
 hadditApp.controller('MainCtrl' ,
-  ['$scope',
-    function($scope){
+  ['$scope', 'postFactory',
+    function($scope, postFactory){
       $scope.test = 'Hello World';
-
-      $scope.posts = [
-        {title: 'post 1', upvotes: 5},
-        {title: 'post 2', upvotes: 2},
-        {title: 'post 3', upvotes: 15},
-        {title: 'post 4', upvotes: 9},
-        {title: 'post 5', upvotes: 4}
-      ];
+      $scope.posts = postFactory.posts;
 
       $scope.addPost = function() {
         if(!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({title: $scope.title, link: $scope.link , upvotes: 2});
+
+        $scope.posts.push({title: $scope.title,
+                          link: $scope.link,
+                          upvotes: 2});
         $scope.title = '';
-        $scope.link = '';
+        $scope.link  = '';
       };
 
       $scope.incrementUpvotes = function(post) {
