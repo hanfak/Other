b502eb074fd888900afeee30f8e93221e4ea361a
Hanif Fakira <fakira.work@gmail.com>
2016-09-07 12:33:13 +0100

    PS railstdd show description of achievement
    Use redcarpet to render
    use factory girl to inherit and overide, and to create lists of diff

--- i-rock/spec/factories/achievements.rb
+++ i-rock/spec/factories/achievements.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
   factory :achievement do
-    title "title"
+    # After each instance of created, changes title, to have several different titles
+    sequence(:title) { |n| "Achievement #{n}"}
     description "description"
     privacy Achievement.privacies[:private_access]
     featured false
     cover_image "some_file.png"
+    # inherit and overirde parts
+    factory :public_achievement do
+      privacy Achievement.privacies[:public_access]
+    end
   end
 end
--- i-rock/spec/features/achievement_page_spec.rb
+++ i-rock/spec/features/achievement_page_spec.rb
@@ -6,5 +6,16 @@ feature "Acheivement page" do
     visit("/achievements/#{achievement.id}")
 
     expect(page).to have_content("go to gym")
+
+    # This creates a list of three achievements
+    # achievements = FactoryGirl.create_list(:achievement, 3)
+  end
+
+  scenario 'render markdown description' do
+    achievement = FactoryGirl.create(:achievement, description: 'That *was* hard')
+    visit("/achievements/#{achievement.id}")
+
+    expect(page).to have_content("<em>was</em>")
+    # expect(page).to have_content("go to gym")
   end
 end
