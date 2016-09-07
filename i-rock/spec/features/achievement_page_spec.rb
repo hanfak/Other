69c7ae5eb07a1346ce801bf9452bde340893f489
Hanif Fakira <fakira.work@gmail.com>
2016-09-07 12:19:23 +0100

    PS railstdd use factory girl to replace create

--- i-rock/spec/features/achievement_page_spec.rb
+++ i-rock/spec/features/achievement_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature "Acheivement page" do
   scenario "achievement public page" do
-    achievement = Achievement.create(title: 'go to gym')
+    achievement = FactoryGirl.create(:achievement, title: "go to gym")
     visit("/achievements/#{achievement.id}")
 
     expect(page).to have_content("go to gym")
