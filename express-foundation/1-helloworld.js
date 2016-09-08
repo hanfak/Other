5eb0502d297b9ba1b453eba0e90c1edab835742f
Hanif Fakira <fakira.work@gmail.com>
2016-09-08 14:53:24 +0100

    helloworld use of wildcard route

--- express-foundation/1-helloworld.js
+++ express-foundation/1-helloworld.js
@@ -7,7 +7,10 @@ var app = express();
 app.get('/home', function(req, res){
   res.end('Hello World!')
 });
-
+//Wild card, means any route will run the callback
+app.get('*', function(req, res){
+  res.end('Hello World!')
+});
 //Set up port, to listen for server
 // app.listen(3000) //in general to access on localhost:3000
 app.listen(process.argv[2] || 3000); // for program
