567239254a2983785f33a5c435b0d710c162fdc0
Hanif Fakira <fakira.work@gmail.com>
2016-09-08 14:51:38 +0100

    Helloworld complete

--- express-foundation/1-helloworld.js
+++ express-foundation/1-helloworld.js
@@ -4,6 +4,13 @@ var express = require('express');
 var app = express();
 
 //Set up GET route for /
-app.get('/', function(req, res){
-
+app.get('/home', function(req, res){
+  res.end('Hello World!')
 });
+
+//Set up port, to listen for server
+// app.listen(3000) //in general to access on localhost:3000
+app.listen(process.argv[2] || 3000); // for program
+
+// to run server
+// $ node <Filename>.js
