a110597a0cfd57cd231423a711281a5b1bf7a64c
Hanif Fakira <fakira.work@gmail.com>
2016-09-08 18:42:53 +0100

    jade, add more routes

--- express-foundation/3-jade/3-jade.js
+++ express-foundation/3-jade/3-jade.js
@@ -3,12 +3,17 @@ var app = express();
 // set jade templates, library
 app.set('view engine', 'jade');
 // where folder of template
-app.set('views', process.argv[3]);
-// app.set('views', __dirname + '/views');
+// app.set('views', process.argv[3]);
+app.set('views', __dirname + '/views');
 
 
 app.get('/home', function(req, res){
   res.render('index', {date: new Date().toDateString()});
+});
+
+// Using json/object to access data for jade template
+app.get('/next', function(req, res){
+  res.render('next', {name: 'Han'});
 });
 
 app.listen(process.argv[2] || 3000);
