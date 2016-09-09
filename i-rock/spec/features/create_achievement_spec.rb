a28e5012d7f03add72f8f769fc073c4886e6e167
Hanif Fakira <fakira.work@gmail.com>
2016-09-09 19:29:36 +0100

    PS railstdd fixed feature tests
    Include sign in first

--- i-rock/spec/features/create_achievement_spec.rb
+++ i-rock/spec/features/create_achievement_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 require_relative '../support/new_achievement_form'
+require_relative '../support/login_form'
 
 feature 'create new achievement' do
   let(:new_achievement_form) { NewAchievementForm.new }
+  let(:login_form) { LoginForm.new }
+  let(:user) {FactoryGirl.create(:user)}
 
   scenario 'create new achievement with date' do
+    login_form.visit_page.login_as(user)
     new_achievement_form.visit_page.fill_in_with(
       title: 'Read a book'
     ).submit
@@ -14,6 +18,7 @@ feature 'create new achievement' do
   end
 
   scenario 'cannot create achievement with invalid data' do
+    login_form.visit_page.login_as(user)
     new_achievement_form.visit_page.submit
 
     expect(page).to have_content("can't be blank")
