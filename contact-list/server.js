8c46c6c168c93b4d0c60f2e3cdf54bb41543d989
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 12:25:10 +0100

    setup static html in express

--- contact-list/server.js
+++ contact-list/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello');
-});
+// app.get('/', function(req, res) {
+//   res.send('hello');
+// });
+
+app.use(express.static(__dirname + "/public"));
 
 app.listen(3000);
 console.log('server running');
