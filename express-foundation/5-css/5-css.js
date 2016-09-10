a02a3b43a6ed06fe2d95dd712d74cbbb728c4d91
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 11:30:32 +0100

    css change dir

--- express-foundation/5-css/5-css.js
+++ express-foundation/5-css/5-css.js
@@ -1,7 +1,8 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use(require('stylus').middleware(process.argv[3] || 'public'));
-app.use(express.static(process.argv[3] || 'public'));
+app.use(require('stylus').middleware(process.argv[3] || path.join(__dirname, 'public/css')));
+app.use(express.static(process.argv[3] || path.join(__dirname, 'public/')));
 
 app.listen(process.argv[2] || 3000);
