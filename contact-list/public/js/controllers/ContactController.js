f6d809de3e4c09590c5964fb0c46d676252a795e
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 15:17:31 +0100

    create dummy data

--- contact-list/public/js/controllers/ContactController.js
+++ contact-list/public/js/controllers/ContactController.js
@@ -1,3 +1,23 @@
 contactListApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
   console.log('hello from controller');
+
+  person1 = {
+    name: 'person 1 name',
+    email: 'person1@email.com',
+    number: '111 111 1111'
+  };
+
+  person1 = {
+    name: 'person 2 name',
+    email: 'person2@email.com',
+    number: '222 222 2222'
+  };
+
+  person1 = {
+    name: 'person 3 name',
+    email: 'person3@email.com',
+    number: '333 333 3333'
+  };
+
+  var contactList = [person1, person2, person3];
 }]);
