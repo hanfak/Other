a08c9009c3b12c648a72b040b85c62eecca65d38
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 16:19:05 +0100

    ang get data from server route

--- contact-list/public/js/controllers/ContactController.js
+++ contact-list/public/js/controllers/ContactController.js
@@ -1,5 +1,12 @@
 contactListApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
   console.log('hello from controller');
 
-  $http.get('/contactList');
+  $http.get('/file')
+    .success(function(response) {
+      console.log('Received data requested from server');
+      $scope.contactList = response;
+    })
+    .error(function(data, status) {
+      console.error('Repos error', status, data);
+    });
 }]);
