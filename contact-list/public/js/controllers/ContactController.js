18be3b87fe525239e8e50bfeadc2e32f6ad0a6bf
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 15:43:10 +0100

    node server res with json from variable

--- contact-list/public/js/controllers/ContactController.js
+++ contact-list/public/js/controllers/ContactController.js
@@ -1,25 +1,5 @@
 contactListApp.controller('AppCtrl', ['$scope','$http', function($scope, $http) {
   console.log('hello from controller');
 
-  person1 = {
-    name: 'person 1 name',
-    email: 'person1@email.com',
-    number: '111 111 1111'
-  };
-
-  person2 = {
-    name: 'person 2 name',
-    email: 'person2@email.com',
-    number: '222 222 2222'
-  };
-
-  person3 = {
-    name: 'person 3 name',
-    email: 'person3@email.com',
-    number: '333 333 3333'
-  };
-
-  var contactList = [person1, person2, person3];
-
-  $scope.contactList = contactList;
+  $http.get('/contactList');
 }]);
--- contact-list/server.js
+++ contact-list/server.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
 
-// app.get('/', function(req, res) {
-//   res.send('hello');
-// });
+var person1 = {
+  name: 'person 1 name',
+  email: 'person1@email.com',
+  number: '111 111 1111'
+};
+
+var person2 = {
+  name: 'person 2 name',
+  email: 'person2@email.com',
+  number: '222 222 2222'
+};
+
+var person3 = {
+  name: 'person 3 name',
+  email: 'person3@email.com',
+  number: '333 333 3333'
+};
+
+var contactList = [person1, person2, person3];
 
 app.use(express.static(__dirname + "/public"));
+
+app.get('/contactList', function(req,res) {
+  console.log('Received GET request');
+  res.json(contactList);
+});
 
 app.listen(3000);
 console.log('server running');
