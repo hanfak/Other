1a181864922183e347e7701db8580a0bd67d983b
Hanif Fakira <fakira.work@gmail.com>
2016-09-10 11:41:02 +0100

    query

---
+++ express-foundation/7-query/7-query.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/search', function(req, res){
+  var query = req.query;
+  res.send(query);
+});
+
+app.listen(process.argv[2] || 3000);
+// visit localhost:3000/search?name=john&age=20
+//will output a json
