5e6d121bc5583dc69f849fbc9f01630d4cf6ebba
Hanif Fakira <fakira.work@gmail.com>
2016-09-11 14:18:38 +0100

    PS railstdd validates unique title for user

--- i-rock/app/models/achievement.rb
+++ i-rock/app/models/achievement.rb
@@ -2,6 +2,8 @@ class Achievement < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true
+  validates :title, uniqueness: true
+
   enum privacy: [:public_access, :private_access, :friends_access]
 
   def description_html
--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -4,10 +4,17 @@ RSpec.describe Achievement, type: :model do
   describe 'validations' do
     it 'requires a ttile' do
       achievement = Achievement.new(title: "")
-      # achievement.valid?
-      #
-      # expect(achievement.errors[:title]).not_to be_empty
+
       expect(achievement.valid?).to be_falsy
+    end
+
+    it 'requires title to be unique for one user' do
+      user = FactoryGirl.create(:user)
+      first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
+      new_achievement = Achievement.new(title: 'First Achievement', user: user)
+
+      expect(new_achievement.valid?).to be_falsy
+
     end
   end
 end
