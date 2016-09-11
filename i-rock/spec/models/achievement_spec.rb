f8ccfdc31c9f7d5dbb1ab92f8f84eca382bb8a41
Hanif Fakira <fakira.work@gmail.com>
2016-09-11 14:12:57 +0100

    PS railstdd alternative validation test

--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe Achievement, type: :model do
   describe 'validations' do
     it 'requires a ttile' do
       achievement = Achievement.new(title: "")
-      achievement.valid?
-
-      expect(achievement.errors[:title]).to include("can't be blank")
+      # achievement.valid?
+      #
+      # expect(achievement.errors[:title]).not_to be_empty
+      expect(achievement.valid?).to be_falsy
     end
   end
 end
