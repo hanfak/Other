88374b35f45635c8b5665b4fb8e34adfae1e5e98
Hanif Fakira <fakira.work@gmail.com>
2016-09-11 14:26:21 +0100

    PS railstdd validate different user same title

--- i-rock/app/models/achievement.rb
+++ i-rock/app/models/achievement.rb
@@ -2,11 +2,21 @@ class Achievement < ActiveRecord::Base
   belongs_to :user
 
   validates :title, presence: true
-  validates :title, uniqueness: true
+  # validates :title, uniqueness: true
+  validate :unique_title_for_one_user
 
   enum privacy: [:public_access, :private_access, :friends_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description).html_safe
   end
+
+  private
+
+    def unique_title_for_one_user
+      existing_achievement = Achievement.find_by(title: title)
+      if existing_achievement && existing_achievement.user == user
+        errors.add(:title, "you cannot have two achievements with same title")
+      end
+    end
 end
--- i-rock/spec/models/achievement_spec.rb
+++ i-rock/spec/models/achievement_spec.rb
@@ -10,10 +10,19 @@ RSpec.describe Achievement, type: :model do
 
     it 'requires title to be unique for one user' do
       user = FactoryGirl.create(:user)
-      first_achievement = FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
+      FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user)
       new_achievement = Achievement.new(title: 'First Achievement', user: user)
 
       expect(new_achievement.valid?).to be_falsy
+    end
+
+    it 'allows different users to have identitical titles' do
+      user1 = FactoryGirl.create(:user)
+      user2 = FactoryGirl.create(:user)
+      FactoryGirl.create(:public_achievement, title: 'First Achievement', user: user1)
+      new_achievement = Achievement.new(title: 'First Achievement', user: user2)
+
+      expect(new_achievement.valid?).to be_truthy 
 
     end
   end
