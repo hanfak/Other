9c6eb0a26e4001475a3a7c50da2d835f00b190fb
Hanif Fakira <fakira.work@gmail.com>
2016-09-16 07:56:36 +0100

    angular route wip

--- contact-list/public/js/app.js
+++ contact-list/public/js/app.js
@@ -1,1 +1,29 @@
-var contactListApp = angular.module('contactListApp', []);
+var contactListApp = angular.module('contactListApp', ['ui.router' ,
+    "ngResource"]);
+
+contactListApp.config(['$stateProvider',"$locationProvider", '$urlRouterProvider',
+function($stateProvider, $locationProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $stateProvider
+  .state("home", {
+      url:      "/",
+      templateUrl: "../index.html",
+      controller:   "AppCtrl"
+    });
+  // $stateProvider
+  //   .state('home', {
+  //     url: '/',
+  //     templateUrl: '../index.html',
+  //     controller: 'AppCtrl'
+  //   });
+    // .state('posts', {
+    // 		url : '/posts/:id',
+    // 		templateUrl : '../javascripts/templates/posts/show.html',
+    // 		controller : 'PostsCtrl',
+    //     resolve: {
+    //       post: ['$stateParams','postFactory', function($stateParams, postFactory){
+    //         return postFactory.get($stateParams.id);
+    //       }]
+    //     }
+    // 	});
+    }]);
